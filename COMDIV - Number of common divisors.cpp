9bf19f11d7f44fece89dbf0e0a2ac4065bfdf946
mdarafat1819 <52718417+mdarafat1819@users.noreply.github.com>
2020-03-24 11:38:40 +0600

    Add files via upload

--- COMDIV - Number of common divisors.cpp
+++ COMDIV - Number of common divisors.cpp
@@ -1,33 +1,36 @@
-#include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int gcd(int a, int b)
+int divisorCount(int n)
 {
-    return !b ? a : gcd(b, a % b);
-}
-int cDivisorCount(int a, int b)
-{
-    int g = gcd(a, b), cnt = 0;
-    for(int i = 1; i * i <= g; i++)
-       {
-           if(i * i == g) cnt++;
-           else if(!(g % i)) cnt += 2;
-       }
+    int cnt = 0;
+    for(int i = 1; i * i <= n; i++)
+    {
+        if(i * i == n) cnt++;
+        else if(!(n % i)) cnt += 2;
+    }
     return cnt;
 }
-
+int gcd(int a, int b)
+{
+    while(1)
+    {
+        if(!a) return b;
+        int temp = a;
+        a = b % a;
+        b = temp;
+    }
+}
 int main()
 {
     int t;
-    cin>>t;
+    scanf("%d", &t);
     while(t--)
     {
-        int a, b, g, cnt = 0;
+        int a, b;
         scanf("%d%d", &a, &b);
-        printf("%d\n", cDivisorCount(a, b));
+        printf("%d\n", divisorCount(gcd(a, b)));
     }
-
     return 0;
 }
