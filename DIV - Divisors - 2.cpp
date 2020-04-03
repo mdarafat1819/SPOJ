099e777ccd302f2f355c99b4baff72f661b81afe
mdarafat1819 <52718417+mdarafat1819@users.noreply.github.com>
2020-04-03 15:03:36 +0600

    Update DIV - Divisors - 2.cpp

--- DIV - Divisors - 2.cpp
+++ DIV - Divisors - 2.cpp
@@ -5,18 +5,15 @@
 using namespace std;
 
 bool np[80];
-int nDiv[SIZE];
+int nDiv[SIZE], ans[SIZE], idx;
 
 int primeFact(int n)
 {
     int cnt = 0, p = 0;
     for(int i = 2; i * i <= n; i++)
     {
-        if(!(n % i))
-        {
-            p++;
-            while(!(n % i)) n /= i, cnt++;
-        }
+        if(!(n % i)) p++;
+        while(!(n % i)) n /= i, cnt++;
     }
     if(n > 1) p++, cnt++;
     if(p == cnt && cnt == 2) return 1;
@@ -25,23 +22,16 @@ int primeFact(int n)
 void sieve()
 {
     for(int i = 1; i < SIZE; i++)
-        for(int j = i; j < SIZE; j += i)
-            nDiv[j]++;
-    for(int i = 1; i < 80; i++)
-        np[i] = primeFact(i);
+        for(int j = i; j < SIZE; j += i) nDiv[j]++;
+    for(int i = 1; i < 80; i++) np[i] = primeFact(i);
 }
 int main()
 {
     sieve();
     int cnt = 0;
-    for(int i = 1; i < SIZE; i++)
-    {
-        if(np[nDiv[i]])
-        {
-            cnt++;
-            if(!(cnt % 9))
-                cout<<i<<endl;
-        }
-    }
+     for(int i = 1; i < SIZE; i++)
+        if(np[nDiv[i]]) ans[idx] = i, idx++;
+     for(int i = 8; i < idx; i += 9)
+        cout<<ans[i]<<endl;
     return 0;
 }
